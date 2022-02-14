b38eb2597d7632e575d01768dbbd07f45715afd0
CodingDragon21 <85629101+CodingDragon21@users.noreply.github.com>
2022-02-14 11:35:54 -0600

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+} from "react-native";
+
+export default class Profile extends Component{
+    render(){
+        return(
+            <View style = {styles.container}>
+                <Text style = {styles.text}>Profile Screen</Text>
+                </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    },
+    text:{
+        aliginSelf: "center",
+        color: 'blue',
+        fontFamily: 'jokerman',
+        marginTop: 30
+
+    }
+})
